8f25b12f06f7312bc644471f723a543ea731a56e
andy <1044375652@qq.com>
2020-05-11 22:36:21 +0800

    :bug: 问题fix

--- index8.js
+++ index8.js
@@ -1,1 +1,11 @@
 console.log(123)
+//  "commitlint": {
+//    "extends": [
+//      "@commitlint/config-conventional"
+//    ]
+//  },
+//  "husky": {
+//    "hooks": {
+//      "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"
+//    }
+//  },
